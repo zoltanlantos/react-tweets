42a925a8c0ca4f07e5dd0cccfc07adb447a268a8
zoltan-lantos <lantos.zoltan@live.com>
2017-05-11 04:20:46 +0100

    Add input field to change twitter handle.

--- src/components/tweet-list.js
+++ src/components/tweet-list.js
@@ -5,24 +5,51 @@ import config from '../config';
 class TweetList extends Component {
   constructor() {
     super();
-    this.state = { tweets: [] };
+    this.state = {
+      tweets: [],
+      screenName: 'cnnbrk',
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount() {
-    fetch(`http://localhost:${config.server.port}/cnnbrk-tweets`)
+    this.getTweets();
+  }
+  getTweets() {
+    fetch(`http://localhost:${config.server.port}/${this.state.screenName}-tweets`)
       .then(result => result.json())
       .then(tweets => this.setState({ tweets }));
   }
+  handleChange(e) {
+    this.setState({ screenName: e.target.value });
+  }
+  handleClick() {
+    this.getTweets();
+  }
   render() {
     return (
-      <div className="grid">
-        { this.state.tweets.length
-          ? this.state.tweets.map(tweet => (
-            <div className="grid__item" key={tweet.id}>
-              <Tweet tweet={tweet} />
-            </div>
-            ))
-          : <div>Loading...</div>
-        }
+      <div>
+        <div>
+          <input
+            type="text"
+            value={this.state.screenName}
+            onChange={this.handleChange}
+            placeholder="Type twitter screen name"
+          />
+          <button
+            onClick={this.handleClick}
+          >Load tweets</button>
+        </div>
+        <div className="grid">
+          { this.state.tweets.length
+            ? this.state.tweets.map(tweet => (
+              <div className="grid__item" key={tweet.id}>
+                <Tweet tweet={tweet} />
+              </div>
+              ))
+            : <div>Loading...</div>
+          }
+        </div>
       </div>
     );
   }
